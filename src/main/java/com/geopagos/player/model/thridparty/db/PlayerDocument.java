22fec47f8cb44284d4766208f95a365e45864bdc
Andres Gonzales <agonzalesr93@gmail.com>
2023-03-19 13:11:44 -0500

    Update PlayerDocument.java

--- src/main/java/com/geopagos/player/model/thridparty/db/PlayerDocument.java
+++ src/main/java/com/geopagos/player/model/thridparty/db/PlayerDocument.java
@@ -5,9 +5,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
+@Document("player")
 @EqualsAndHashCode
 public class PlayerDocument {
 
